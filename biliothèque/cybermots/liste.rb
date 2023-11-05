dcdd117e40034c790551277dfbb9fe5ab134b516
noraj <alexandre.zanni@europe.com>
2023-11-05 17:23:54 +0100

    meilleur doc

--- biliothèque/cybermots/liste.rb
+++ biliothèque/cybermots/liste.rb
@@ -6,21 +6,25 @@ require 'json'
 module CyberMots
   class Liste
     # Liste les mots français dans l'ordre alphabétique
+    # @return [Array<String>] mots
     def self.fr
       liste(:fr)
     end
 
     # Liste les mots anglais dans l'ordre alphabétique
+    # @return [Array<String>] mots
     def self.en
       liste(:en)
     end
 
     # Liste les correspondances français vers anglais dans l'ordre alphabétique
+    # @return [Array<String>] mots et leur traduction
     def self.fren
       liste(:fren)
     end
 
     # Liste les correspondances anglais vers français dans l'ordre alphabétique
+    # @return [Array<String>] mots et leur traduction
     def self.enfr
       liste(:enfr)
     end
